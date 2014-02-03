a15cf61a7218d87f28d9dc4da8fcb0371a86fc79
Kai Schneider <schneikai@gmail.com>
2014-02-03 16:37:22 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paasrails::Application.config.secret_key_base = '7441ea7189d1c4b24a944b1b1c9fb7c5d067e72f3da133064dacd7a26724979e8ffbfb72ed0e1f61f08f3e918db8cf656cb0b3af23be424741b980520d26b541'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Paasrails::Application.config.session_store :cookie_store, key: '_paasrails_session'
